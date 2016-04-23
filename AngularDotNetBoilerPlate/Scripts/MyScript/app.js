5846c75112890b58448abc659cd6597999a429b1
Zaeem <zaeemabbas20@hotmail.com>
2016-04-24 01:50:33 +0500

    applied routing and set basic structure for angular folders

---
+++ AngularDotNetBoilerPlate/Scripts/MyScript/app.js
@@ -0,0 +1,38 @@
+﻿var app = angular.module('app', ['ngRoute']);
+
+
+/****************** Controller for Home Page *******************/
+app.controller('homeController', function ($scope, $location) {
+
+    /****************** On Load *******************/
+    $scope.onLoad = function () {
+        $scope.welcome = "Welcome to Angular plus Dotnet Demo.";
+    }
+
+    /***************AJAX Call  *****************/
+    function makeAjaxCall(parameter) {
+        $.ajax({
+            type: 'POST',
+            url: "../controls/login.ashx?methodName=" + parameter.method,
+            data: parameter,
+            dataType: 'json',
+            success: function (response) {
+                if (response.IsSuccess) {
+                    eval(response.CallBack + '(response.Data, response.Message)');
+                }
+                else {
+                    swal({
+                        title: "ERROR",
+                        text: response.Message,
+                        type: "error",
+                        confirmButtonClass: 'btn-danger',
+                    });
+                }
+            },
+            error: function () {
+
+            }
+        })
+    }
+});
+
---
+++ AngularDotNetBoilerPlate/Scripts/MyScript/routing.js
@@ -0,0 +1,25 @@
+﻿/// <reference path="../angular.min.js" />
+/// <reference path="../angular-route.min.js" />
+
+
+/*********** Angular Routing ***********/
+
+// configure our routes
+app.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+
+        // route for the employee page
+        .when('/', {
+            templateUrl: 'Pages/home.html',
+            controller: 'homeController',
+        })
+
+        .otherwise({
+            redirectTo: '/'
+        });
+
+    /*******For Clean URLs********/
+    $locationProvider.html5Mode(true);
+
+});
+
